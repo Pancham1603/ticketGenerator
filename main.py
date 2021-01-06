34cc579158a807dcb771ae8fe0d29adb0cd35fce
PanchamAgarwal <pancham1603@gmail.com>
2021-01-06 16:47:32 +0530

    added email verification

--- main.py
+++ main.py
@@ -1,49 +1,92 @@
 import mysql.connector
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session
 import random as rd
 import pyqrcode
 from PIL import Image
 from pyzbar.pyzbar import decode
+import smtplib
+import config
 
 app = Flask(__name__)
+app.secret_key = '***REMOVED***'
 
 db = mysql.connector.connect(host="localhost",
                              user="root",
                              password="***REMOVED***",
                              database="***REMOVED***")
 
+
+def sendmail(receiver, message):
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+    server.sendmail(config.EMAIL_ADDRESS, receiver, message)
+    server.quit()
+
+
 @app.route('/')
 def welcome():
     return render_template('index.html')
 
-@app.route('/addparticipant', methods=['GET', 'POST'])
-def addparticipant():
-    if request.method == 'POST':
-        participantDetails = request.form
-        name = participantDetails['name']
-        email = participantDetails['email']
-        grade = participantDetails['grade']
-        contact = participantDetails['contact']
-        school = participantDetails['school']
 
+@app.route('/registrationform', methods=['GET', 'POST'])
+def addparticipant():
+    return render_template('addParticipant.html')
+
+
+@app.route('/addparticipant', methods=['POST'])
+def participantdatasubmit():
+    participantDetails = request.form
+    session['name'] = participantDetails['name']
+    session['email'] = participantDetails['email']
+    session['grade'] = participantDetails['grade']
+    session['contact'] = participantDetails['contact']
+    session['school'] = participantDetails['school']
+    session['TFA'] = rd.randint(100000, 999999)
+    subject = 'TICKET GENERATOR VERIFICATION TEST'
+    msg = f"""
+    Greetings,
+
+        Test Mail
+        Your verification code is {session['TFA']}
+
+    Regards,
+    Team CodeTech
+    Birla Vidya Niketan
+    """
+    message = 'Subject: {}\n\n{}'.format(subject, msg)
+    print(f"Sending verification mail to {session['name']} ")
+    sendmail(participantDetails['email'], message)
+    return render_template('verification.html')
+
+
+@app.route('/addparticipant/confirmation', methods=['POST'])
+def verifycommit():
+    verifdata = request.form
+    print(session['name'])
+    print(session['TFA'])
+    print(verifdata['verification_code'])
+    if int(session['TFA']) == int(verifdata['verification_code']):
         num = rd.randint(100000, 999999)
         qrembed = f"CODEx2021_{num}"
 
         code = pyqrcode.create(qrembed)
-        code.png(f"/Users/pancham/PycharmProjects/flasktestapp/QRCodes/CODEx2021_{name}.png", scale=8)
+        code.png(f"/Users/pancham/PycharmProjects/flasktestapp/QRCodes/CODEx2021_{session['name']}.png", scale=8)
 
-        with open(f"/Users/pancham/PycharmProjects/flasktestapp/QRCodes/CODEx2021_{name}.png", 'rb') as file:
+        with open(f"/Users/pancham/PycharmProjects/flasktestapp/QRCodes/CODEx2021_{session['name']}.png", 'rb') as file:
             binaryData = file.read()
-
         cur = db.cursor()
-        cur.execute("INSERT INTO participantData(name,email,class,contact,school,CODExID,qrcode) VALUES(%s,%s,%s,%s,"
-                    "%s,%s,%s)",
-                    (name, email, grade, contact, school, qrembed, binaryData))
+        cur.execute(
+            "INSERT INTO participantData(name,email,class,contact,school,CODExID,qrcode) VALUES(%s,%s,%s,%s,"
+            "%s,%s,%s)",
+            (session['name'], session['email'], session['grade'], session['contact'], session['school'], qrembed,
+             binaryData))
         db.commit()
         cur.close()
-        return 'success'
-    return render_template('addParticipant.html')
-
+        return 'You have successfully registered!'
+    else:
+        return "Invalid verification code, please register again."
 
 @app.route('/scanparticipant', methods=['GET', 'POST'])
 def scanparticipant():
@@ -55,8 +98,8 @@ def scanparticipant():
         cur = db.cursor()
         cur.execute('SELECT * FROM participantData WHERE CODExID like' + f"'%{targetParticipant}%';")
         data = cur.fetchall()
-        if len(data)>0:
-            return render_template('scan-results.html',data=data)
+        if len(data) > 0:
+            return render_template('scan-results.html', data=data)
     return render_template('scanParticipant.html')
 
 
@@ -66,7 +109,7 @@ def participants():
     cur.execute('SELECT * FROM participantData;')
 
     userdetails = cur.fetchall()
-    if len(userdetails)>0:
+    if len(userdetails) > 0:
         return render_template('users.html', userdetails=userdetails)
 
 
@@ -82,13 +125,15 @@ def searchbyname():
             return render_template('search results.html', searchdata=searchdata)
     return render_template('searchbyname.html')
 
+
 @app.errorhandler(404)
 def error404(error):
-    return "<h1>ERROR 404</h1>",404
+    return "<h1>ERROR 404</h1>", 404
+
 
 @app.errorhandler(500)
 def error404(error):
-    return "<h1>ERROR 500</h1>",500
+    return "<h1>ERROR 500</h1>", 500
 
 
 if __name__ == "__main__":
